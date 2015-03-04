5a3ef14475e850b6e1b52dbd5008892ef2ce61ba
Halan Pinheiro <halan.pinheiro@gmail.com>
2015-03-04 17:38:25 -0300

    type error

--- m.js
+++ m.js
@@ -2,6 +2,6 @@
   var
     mds = {},
     def = function(name, deps, module){ mds[name] = {m: module, d: deps}; },
-    req = function(name){ m = mds[name]; return $m[m].apply(window, m[d].map(function(dep){ return req(dep); })); };
+    req = function(name){ m = mds[name]; return m[m].apply(window, m[d].map(function(dep){ return req(dep); })); };
   window.M = { define: def, require: req };
 })();
