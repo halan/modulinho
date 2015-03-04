9e58834b4c43465616c964431e533441096cddb6
Halan Pinheiro <halan.pinheiro@gmail.com>
2015-03-04 17:37:48 -0300

    Add dependencies

--- m.js
+++ m.js
@@ -1,7 +1,7 @@
 (function(){
   var
-    modules = {},
-    define = function(name, module){ modules[name] = module; },
-    require = function(name){ return modules[name](); };
-  window.M = { define: define, require: require };
+    mds = {},
+    def = function(name, deps, module){ mds[name] = {m: module, d: deps}; },
+    req = function(name){ m = mds[name]; return $m[m].apply(window, m[d].map(function(dep){ return req(dep); })); };
+  window.M = { define: def, require: req };
 })();
